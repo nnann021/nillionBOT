214bd4646108cc3ef1d6f21c874c14bc94fad014
nnann021 <161106222+nnann021@users.noreply.github.com>
2024-06-09 17:29:45 +0700

    Update sendTransaction.js

--- src/sendTransaction.js
+++ src/sendTransaction.js
@@ -19,7 +19,7 @@ async function sendTransaction(wallet) {
   const recipient = await getRecipientAddress();
   const amount = coins(Math.floor(Math.random() * 10) + 1, 'unil');
 
-  console.log(`Send 0.00${amount[0].amount} $NIL to`.yellow, recipient);
+  console.log(`Send 0.0${amount[0].amount} $NIL to`.yellow, recipient);
 
   const [firstAccount] = await wallet.getAccounts();
 
